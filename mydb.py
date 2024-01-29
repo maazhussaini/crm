8a75e459654b6deefad1787ca4d9a9d07219e0eb
maazhussaini <skr2570@gmail.com>
2024-01-29 18:16:48 +0500

    change in db config

--- mydb.py
+++ mydb.py
@@ -1,8 +1,7 @@
 import mysql.connector as sql
 connection = sql.connect(host = 'localhost',
             user = 'root',
-            password = '1234',
-            database = 'advisors')
+            password = '1234')
 
 cursor = connection.cursor()
 cursor.execute("CREATE DATABASE crm")
